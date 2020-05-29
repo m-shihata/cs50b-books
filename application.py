8d71aee7ce2917e946de1aed5ea508facc980247
MuhammadShihata <mushihata@gmail.com>
2020-05-29 03:01:02 +0200

    Improved version of books

--- application.py
+++ application.py
@@ -23,14 +23,14 @@ def books():
     if not get("search_input"):
         return render_template("error.html", message="You must type something!")
 
-    books = db.session.query(Author, Book).filter(Author.id ==
-                Book.author_id).filter(Book.title.like(f"%{s}%") | Author.name.like(f"%{s}%")).all()
+    books = Book.query.filter(Book.title.like(f"%{s}%")).order_by(Book.title).all()
+    authors = Author.query.filter(Book.title.like(f"%{s}%")).order_by(Author.name).all()
 
     # Make sure flight is valid.
-    if len(books) == 0:
+    if len(books) == 0 and len(authors) == 0:
         return render_template("error.html", message="No results found")
 
-    return render_template("books.html", books=books)
+    return render_template("books.html", books=books, authors=authors)
 
 
 @app.route("/book/<int:book_id>")
@@ -38,5 +38,12 @@ def book(book_id):
     book = Book.query.get(book_id)
     author = book.author
     return render_template("book.html", book=book, author=author)
+
+
+@app.route("/author/<int:author_id>")
+def author(author_id):
+    author = Author.query.get(author_id)
+    books = Book.query.filter_by(author_id = author_id).order_by(Book.title).all()
+    return render_template("author.html", author=author, books=books)
 
 
--- models.py
+++ models.py
@@ -6,12 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Student(db.Model):
-#     __tablename__ = "students"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String, nullable=False) 
-
-
 class Author(db.Model):
     __tablename__ = "authors"
     id = db.Column(db.Integer, primary_key=True)
@@ -19,22 +13,10 @@ class Author(db.Model):
 
 
 class Book(db.Model):
-    __tablename__ = "books" 
+    __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String, nullable=False)
     title = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("authors.id"), nullable=False)
     author = db.relationship("Author", backref="book", lazy=True)
-    # borrowers = db.relationship("Borrower", backref="book", lazy=True)
-
-    # def add_borrower(self, name):
-    #     b = Borrower(name=name, book_id=self.id)
-    #     db.session.add(b)
-    #     db.session.commit()
-
-
-# class Borrower(db.Model):
-#     __tablename__ = "borrowers"
-#     student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
-#     book_id = db.Column(db.Integer, db.ForeignKey("books.id"), nullable=False)
